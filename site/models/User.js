b26ad9aabcb380a7e861b83f909f596fd3214088
William Shiao <willshiao@gmail.com>
2017-05-20 10:41:55 -0700

    Finish user login and registration

--- site/models/User.js
+++ site/models/User.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const mongoose = require('mongoose');
+const mongoose = require('../lib/db');
 const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
--- site/routes/index.js
+++ site/routes/index.js
@@ -3,38 +3,53 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
 const User = require('../models/User');
-const logger = require('./lib/logger');
+const logger = require('../lib/logger');
 
 router.use(bodyParser.urlencoded({extended: false}));
 
 router.post('/login', (req, res) => {
-  if(!res.body) return res.failMsg('Invalid information');
-  if(!res.body.username || !res.body.password) return res.failMsg('Username and password required.');
-  if(res.cookies.user) return res.failMsg('Already logged in.');
+  if(!req.body) return res.failMsg('Invalid information');
+  if(!req.body.username || !req.body.password) return res.failMsg('Username and password required.');
+  if(req.cookies.user) return res.failMsg('Already logged in.');
 
-  User.findOne({username: res.body.username}).lean().exec()
+  User.findOne({username: req.body.username}).lean().exec()
     .then((user) => {
-      user.comparePassword(res.body.password, (valid) => {
+      if(!user) return res.failMsg('Invalid username');
+      user.comparePassword(req.body.password, (valid) => {
         if(!valid) return res.failJson();
         res.cookie('user', user);
         res.successJson();
       });
+    })
+    .catch(err => {
+      logger.error(err);
+      res.errorJson({err: err});
     });
 });
 
 router.post('/register', (req, res) => {
-  if(!res.body) return res.failMsg('Invalid information');
-  if(!res.body.username || !res.body.password) return res.failMsg('Username and password required.');
-  if(res.cookies.user) return res.failMsg('Already logged in.');
+  if(!req.body) return res.failMsg('Invalid information');
+  if(!req.body.username || !req.body.password) return res.failMsg('Username and password required.');
+  if(req.cookies.user) return res.failMsg('Already logged in.');
+  if(req.body.password !== req.body['confirm-password']) return res.failMsg('Password not confirmed');
 
   // No need to check username, password - assume it's nice input
-  const user = new User({
-    username: res.body.username,
-    password: res.body.password,
-  });
-  logger.info('New user: ', user);
+  const userInfo = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  const user = new User(userInfo);
+  user.save()
+    .then(() => {
+      logger.info('New user: ', userInfo);
 
-  res.cookie('user', user);
+      res.cookie('user', userInfo);
+      res.successJson();
+    });
+});
+
+router.get('/userinfo', (req, res) => {
+  res.successJson(req.cookies.user.username);
 });
 
 module.exports = router;
