f2b2d1f4a2a9634e75079c505f4fb4cd1dbcc9dc
William Shiao <willshiao@gmail.com>
2017-05-20 09:46:14 -0700

    Make index route default route

--- site/routes/pages.js
+++ site/routes/pages.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const path = require('path');
 
 router.get('/:pageName', (req, res) => {
-  console.log('Loading page: ', path.join('pages/', req.params.pageName + '.html'));
-  const rs = fs.createReadStream(path.join('pages/', req.params.pageName + '.html'));
+  console.log('Loading page: ', path.join('pages/', (req.params.pageName || 'index') + '.html'));
+  const rs = fs.createReadStream(path.join('pages/', (req.params.pageName || 'index') + '.html'));
 
   rs.on('error', err => {
     return res.json({error: err});
